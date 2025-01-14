21fe2d886963995b83adae66fc6f37bd1b21b9b8
Hyukay <Runebrun3@hotmail.com>
2025-01-14 10:13:24 -0500

    Description modification

--- src/consts.ts
+++ src/consts.ts
@@ -10,7 +10,7 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION: "Welcome to my blog and portfolio.",
 };
 
 export const BLOG: Metadata = {
